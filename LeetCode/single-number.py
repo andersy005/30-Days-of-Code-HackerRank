f144a8517a9f5c3acbc5e7fd02c4d90bc6234481
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-09-03 19:14:53 -0500

    single number

---
+++ LeetCode/single-number.py
@@ -0,0 +1,16 @@
+"""Given an array of integers, every element appears twice except for one. Find that single one.
+Note:
+Algorithm should have a linear runtime complexity. Could you implement it without using extra memory?"""
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ret = nums[0]
+        for i in range(1,len(nums)):
+            ret = ret ^ nums[i]
+            
+            
+        return ret 
