267a41dd3c059664616a651e54a2a8559e139c65
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-10-10 18:56:28 -0500

    Python Lists

--- HackerRank/Python/Basic-Data-Types/lists.py
+++ HackerRank/Python/Basic-Data-Types/lists.py
@@ -1,12 +1,30 @@
-T = int(raw_input().strip())
-L = []
-for t in range(T):
-    args = raw_input().strip().split(" ")
+T = int(input().strip())
+Arr = []
+
+for i in range(T):
+    # save each command as a list 
+    args = input().strip().split(" ")
+    
+    # check for print command
     if args[0] == "print":
-        print L
+        print(Arr)
+        
+    # Check for insert command and insert item to the list
     elif len(args) == 3:
-        getattr(L, args[0])(int(args[1]), int(args[2]))
+        
+        # use getattr (http://devdocs.io/python~3.5/library/functions#getattr)
+        getattr(Arr, args[0])(int(args[1]), int(args[2]))
+        
+        
+    # Check for append
     elif len(args) == 2:
-        getattr(L, args[0])(int(args[1]))
+        getattr(Arr,args[0])(int(args[1]))
+        
+    
+    # Check for the remaining commands such as sort and pop, etc
     else:
-        getattr(L, args[0])()
+        getattr(Arr, args[0])()
+    
+        
+    
+
