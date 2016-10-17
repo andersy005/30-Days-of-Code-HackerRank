2b95c5c6c2dba5cf8a971aafbb915434ad2da46d
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-10-16 23:45:55 -0500

    Insertion at the beginning of the linked list

--- GeekforGeeks/DS/LinkedList/Singly LinkedList/intro/implementation.py
+++ GeekforGeeks/DS/LinkedList/Singly LinkedList/intro/implementation.py
@@ -22,6 +22,13 @@ class LinkedList:
             print temp.data
             temp = temp.next
 
+    # Function that insert a new node at the beginning
+    def insertatBeginning(self, new_data):
+
+        new_node = Node(new_data)
+        new_node.next = self.head   # Make next of new node as head
+        self.head = new_node        # Make the head to point to new Node 
+
 
 if __name__ == "__main__":
 
@@ -34,5 +41,10 @@ if __name__ == "__main__":
 
     llist.head.next = second
     second.next = third
-
+    print"#" * 50
     llist.printList()
+
+    print"#" * 50
+    llist.insertatBeginning(0)
+    llist.printList()
+
